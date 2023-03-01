76291341f3fbc4bc048c52d4f434c0422170bad1
mobinSaremi <117805350+mobinSaremi@users.noreply.github.com>
2023-03-01 14:02:07 +0330

    Update personal.blade.php

--- resources/views/site/blocks/personal.blade.php
+++ resources/views/site/blocks/personal.blade.php
@@ -15,7 +15,7 @@
 						<div class="col-lg-4 col-md-4 col-sm-6 mb-4">
 							<div class="our-team" data-aos="fade-up" data-aos-duration="1200">
 								<img src="{{asset('assets/upload/'.$personal->avatar)}}" class="img-responsive" alt="" />
-								<h4><a href="landing-one.html#">{{$personal->fullname}} </a></h4>
+								<h4>{{$personal->fullname}}</h4>
 								<span class="designation bg-success">{{$personal->post}} </span>
 								<p>{!! $personal->description !!}</p>
 								<ul class="our-team-profile">
